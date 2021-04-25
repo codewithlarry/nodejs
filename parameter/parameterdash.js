fc83ebd135a45b31564395318d5ce0f7d7b7c42e
larrylin <larrylin17@gmail.com>
2021-04-25 10:02:12 -0700

    Fixed all eslint and prettier issues.

--- parameter/parameterdash.js
+++ parameter/parameterdash.js
@@ -1,3 +1,3 @@
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require("minimist")(process.argv.slice(2));
 console.log(argv);
-console.log("port=%d, env=%s, logLevel=%s", argv['p'], argv['e'], argv['l']);
+console.log("port=%d, env=%s, logLevel=%s", argv["p"], argv["e"], argv["l"]);
