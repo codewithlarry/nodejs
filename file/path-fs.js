e30de8f5df0fe24709cc8ad481941a580e51bff8
larrylin <larrylin17@gmail.com>
2021-04-27 16:43:48 -0700

    apply fs.promises to return promise object

--- file/path-fs.js
+++ file/path-fs.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
 const fsExtra = require('fs-extra');
 
 const parsePath = (filepath) =>{
@@ -14,7 +15,8 @@ const findAbsolutePath = (relativePath) =>{
     return path.resolve(relativePath);
 };
 
-const readFile = (filepath) => {
+const readFile = async (filepath) => {
+    /*
     fs.readFile(filepath, 'utf8', (err,data) => {
         if (err) {
             console.error(err)
@@ -22,6 +24,12 @@ const readFile = (filepath) => {
           }
           console.log(data);
     });
+    */
+
+   const content = await fs.promises.readFile(filepath, 'utf8')
+   .catch(err => console.error("Failed to read file", err));
+   
+   return content;
 };
 
 const writeFile = (filepath, content) => {
@@ -42,10 +50,11 @@ const appendFile = (filepath, content) => {
     });
 };
 
-const getFileSize = (filepath) => {
-    fs.stat(filepath, (err,stat) => {
-       console.log("size="+stat.size);
-    });
+const getFileInfo = async (filepath) => {
+    const stat = await fs.promises.stat(filepath)
+    .catch(err => console.error("Failed to read file", err));
+    return stat;
+
 };
 
 const createFolder = (folderName) => {
@@ -54,9 +63,15 @@ const createFolder = (folderName) => {
     }
 }
 
-const listAllFiles = (folderpath) => {
+const listFiles = (folderpath) => {
+    /*
     fs.readdirSync(folderPath).map(fileName => {
-        return path.join(folderPath, fileName)});
+        return path.join(folderPath, fileName);
+    });*/
+
+    const fileList = fs.promises.readdir(folderpath)
+    .catch(err => console.error("Failed to read dir", err));
+    return fileList;
 };
 
 const copyFolder = async (folderpathSrc, folderpathDest) => {
@@ -83,13 +98,25 @@ console.log(JSON.stringify(pathobj));
 let absolutePath = findAbsolutePath("./file/test.txt");
 console.log(absolutePath);
 
-readFile("./file/test.txt");
+readFile("./file/test.txt").then(content => {
+    console.log("content="+content);
+});
+
 
 writeFile("./file/testwrite.txt", "codewithlarry helps you improve full stack skills.");
 
 appendFile("./file/testwrite.txt","\nyou will become a senior developer soon");
 
-getFileSize("./file/test.txt");
+getFileInfo("./file/test.txt").then(stat => {
+    console.log("file size = "+ stat.size);
+    console.log("Is a file = "+ stat.isFile());
+});
+
+listFiles("./file").then(fileList =>{
+   console.log(JSON.stringify(fileList, null, '  '));
+});
+
+createFolder("./tempdir");
 
 copyFolder("./file","./file-bak-1");
 
